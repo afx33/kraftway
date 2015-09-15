878506549f56b81e32868333338618c67169b413
afx33 <afx33@gmail.com>
2015-09-15 18:03:13 +0000

    file_creator: g++ compiled version

--- file_creator/file_creator/main.cpp
+++ file_creator/file_creator/main.cpp
@@ -12,25 +12,16 @@ using namespace std;
 
 int main()
 {
-	/*int m[100]={0};
-    std::srand((unsigned)time(NULL));  
-   
-    for(int i = 0; i < 100; i++)
-    { 
-		m[i] = rand()%10;
-		std::cout << m[i] << ' ';            
-    }
-    cout << '\n';*/
 	uint32_t m[30] = { 10, 12, 13, 9, 5, 6, 8, 50, 25, 40, 32, 9, 75, 28, 10, 12, 17, 36, 35, 33, 34, 15, 1, 5, 6, 19, 40, 42, 37, 12 };
     
 	std::ofstream file;
 	file.open("unsorted", std::ofstream::binary);	
 	if (file.is_open()) {
-		for(int i = 0; i < sizeof(m)/sizeof(uint32_t); i++) {
+		for(unsigned i = 0; i < sizeof(m)/sizeof(uint32_t); i++) {
 			file.write((char *)&m[i], sizeof(int)); 
 		}
 	}
 	
-    file.close();
+    	file.close();
 	return 0;
 }
